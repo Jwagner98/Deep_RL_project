328b2153934044cec073ef996d9c15eb2d111d59
Joseph <joewag98@gmail.com>
2024-12-02 15:49:11 -0600

    nick! push your code

--- Online_project_RL.py
+++ Online_project_RL.py
@@ -11,11 +11,14 @@ from poke_env.environment import SideCondition
 from poke_env.player import Gen9EnvSinglePlayer, RandomPlayer, SimpleHeuristicsPlayer, Player
 from poke_env import AccountConfiguration, ShowdownServerConfiguration
 from poke_env.player.battle_order import BattleOrder, ForfeitBattleOrder
+#import tqdm
 GEN_9_DATA = GenData.from_gen(9)
+
 class OnlineAgent(Player):
     def __init__(self, model, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model = model
+    
     def embed_battle(self, battle):
         # -1 indicates that the move does not have a base power
         # or is not available
@@ -40,29 +43,97 @@ class OnlineAgent(Player):
             len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
         )
 
+        # hp
+        remaining_health = np.ones(6)
+        opponent_remaining_health = np.ones(6)
+        # stats
+        stats = np.zeros(7)
+        opponent_stats = np.zeros(7)
+        # status
+        status = np.zeros(6)
+        opponent_status = [mon.status for mon in battle.opponent_team.values()]
+        for i, mon in enumerate(battle.team.values()):
+            remaining_health[i] = mon.current_hp_fraction
+            if mon.status is not None:
+                status[i] = mon.status.value
+            if mon.active:
+                for j,b in enumerate(mon.boosts.values()):
+                    stats[j] = b
+        for i, mon in enumerate(battle.opponent_team.values()):
+            opponent_remaining_health[i] = mon.current_hp_fraction
+            if mon.status is not None:
+                opponent_status[i] = mon.status.value
+            if mon.active:
+                for j,b in enumerate(mon.boosts.values()):
+                    opponent_stats[j] = b
+
+        # tera
+        tera = bool(battle.can_tera)
+        opponent_tera = bool(battle._opponent_can_terrastallize)
+
+        # hazards
+        hazarddex = { # relevant side conditions to track (to reduce this dimension by half and not have variable sized input)
+            SideCondition.UNKNOWN:0,
+            SideCondition.AURORA_VEIL:1,
+            SideCondition.LIGHT_SCREEN:2,
+            SideCondition.MIST:3,
+            SideCondition.REFLECT:4,
+            SideCondition.SAFEGUARD:5,
+            SideCondition.SPIKES:6,
+            SideCondition.STEALTH_ROCK:7,
+            SideCondition.STICKY_WEB:8,
+            SideCondition.TAILWIND:9,
+            SideCondition.TOXIC_SPIKES:10,
+        }
+        hazards = np.zeros(11)
+        for cond,val in battle.side_conditions.items():
+            if cond in hazarddex:
+                hazards[hazarddex[cond]] = val
+        opponent_hazards = np.zeros(11)
+        for cond,val in battle.opponent_side_conditions.items():
+            if cond in hazarddex:
+                opponent_hazards[hazarddex[cond]] = val
+
+        # terrain
+        fields = np.zeros(13)
+        for f,val in battle.fields.items():
+            fields[f.value-1] = val
+
+        # weather
+        weather = np.zeros(9)
+        for w,val in battle.weather.items():
+            weather[w.value-1] = val
+
+        # turn
+        turn = battle.turn
+
         # Final vector with 10 components
         return np.concatenate(
             [
-                moves_base_power,
-                moves_dmg_multiplier,
-                [remaining_mon_team, remaining_mon_opponent],
+                moves_base_power, # 4* -1,3
+                moves_dmg_multiplier, # 4* 0,4
+                [remaining_mon_team, remaining_mon_opponent], # 1*1* 0,1 0,1
+                remaining_health, # 6* 0,1
+                opponent_remaining_health, # 6* 0,1
+                [tera, opponent_tera], # 1*1* 0,1 0,1
+                stats, # 7* -6,6
+                opponent_stats, # 7* -6,6
+                hazards, # 11* 0,MAX_TURNS
+                opponent_hazards, # 11* 0,MAX_TURNS
+                fields, # 13* 0,MAX_TURNS
+                weather, # 9* 0,MAX_TURNS
+                [turn], # 1* 0,MAX_TURNS
             ]
         )
-
-    def calc_reward(self, last_state, current_state) -> float:
-
-        return self.reward_computing_helper(
-            current_state, fainted_value=2, hp_value=1, victory_value=30
-        )
-
+    
     def describe_embedding(self):
-        low = [-1, -1, -1, -1, 0, 0, 0, 0, 0, 0]
-        high = [3, 3, 3, 3, 4, 4, 4, 4, 1, 1]
+        low = np.concatenate([[-1]*4, [0]*4, [0]*2, [0]*6, [0]*6, [0]*2, [-6]*7, [-6]*7, [0]*11, [0]*11, [0]*13, [0]*9, [0]])
+        high = np.concatenate([[3]*4, [4]*4, [1]*2, [1]*6, [1]*6, [1]*2, [6]*7, [6]*7, [MAX_TURNS]*11, [MAX_TURNS]*11, [MAX_TURNS]*13, [MAX_TURNS]*9, [MAX_TURNS]])
         return Box(
-            np.array(low, dtype=np.float32),
-            np.array(high, dtype=np.float32),
-            dtype=np.float32,
-        )   
+            np.array(low, dtype=np.float64),
+            np.array(high, dtype=np.float64),
+            dtype=np.float64,
+        )
     
     def action_to_move(self, action, battle):
         if action == -1:
@@ -115,24 +186,25 @@ class OnlineAgent(Player):
         obs = self.embed_battle(battle)
         action, _ = self.model.predict(obs, deterministic=True)
         return self.action_to_move(action,battle)
-    
+
 async def main():
     # We create a random player
-    player = OnlineAgent(model=DQN.load("dqn_model_30k"),
+    player = OnlineAgent(
         account_configuration=AccountConfiguration("RL_baseline", "Wagner777"),
-        server_configuration=ShowdownServerConfiguration,
+        server_configuration=ShowdownServerConfiguration,model=A2C.load("a2c_model_3"),
     )
     # Sending challenges to 'your_username'
     #await player.send_challenges("Joe_wag_RL", n_challenges=1)
 
     # Accepting one challenge from any user
     #await player.accept_challenges(None, 1)
+    await player.accept_challenges('Joe_wag_RL', 10)
 
     # Accepting three challenges from 'your_username'
-    await player.accept_challenges('Joe_Wag_RL', 5)
+    # await player.accept_challenges('your_username', 3)
 
     # Playing 5 games on the ladder
-    #await player.ladder(5)
+    # await player.ladder(5)
 
     # Print the rating of the player and its opponent after each battle
     # for battle in player.battles.values():
